c6e2fae0b7c92c96f6a23deed4b88a9c300a99bb
Tyler Ham <tyler@thamtech.com>
2015-11-04 09:36:59 -0600

    Fix incorrect namespace in UuidValidator

--- src/validators/UuidValidator.php
+++ src/validators/UuidValidator.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * Copyright (C) 2015 Thamtech, LLC
- * 
+ *
  * This software is copyrighted. No part of this work may be
  * reproduced in whole or in part in any manner without the
  * permission of the Copyright owner, unless specifically authorized
  * by a license obtained from the Copyright owner.
 **/
 
-namespace thamtech\uuid\validators\UuidValidator;
+namespace thamtech\uuid\validators;
 
 use yii\validators\RegularExpressionValidator;
 
@@ -16,7 +16,7 @@ use yii\validators\RegularExpressionValidator;
  * UuidValidator validates that the attribute value matches the pattern of
  * a canonical format UUID, i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a.
  * The comparison is case insensitive.
- * 
+ *
  * @author Tyler Ham <tyler@thamtech.com>
  */
 class UuidValidator extends RegularExpressionValidator
